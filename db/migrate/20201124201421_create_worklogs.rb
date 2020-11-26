130a22afbefd5e62f8a71c19619e659ce1208917
Çağrı Özkan <cagrio.dev@gmail.com>
2020-11-26 22:34:00 +0300

    Rename worklog attribute👀

--- db/migrate/20201124201421_create_worklogs.rb
+++ db/migrate/20201124201421_create_worklogs.rb
@@ -1,7 +1,7 @@
 class CreateWorklogs < ActiveRecord::Migration[6.0]
   def change
     create_table :worklogs do |t|
-      t.string :comment
+      t.string :remark
       t.integer :time_type
       t.datetime :starting_at
       t.datetime :ending_at
