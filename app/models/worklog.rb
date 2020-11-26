cb053149238e65a76f2d2c4fc7395e510a5797d9
Çağrı Özkan <cagrio.dev@gmail.com>
2020-11-26 22:36:38 +0300

    Add validations

--- app/models/worklog.rb
+++ app/models/worklog.rb
@@ -1,2 +1,6 @@
 class Worklog < ApplicationRecord
+  validates_presence_of :starting_at
+  validates_presence_of :ending_at
+  validates_presence_of :time_type
+  enum time_type: [ :self_improvement, :dayjob, :side_project ]
 end
---
+++ spec/models/worklog_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Worklog, type: :model do
+  describe 'model consistency' do
+    it { is_expected.to validate_presence_of(:starting_at) }
+    it { is_expected.to validate_presence_of(:time_type) }
+    it { is_expected.to validate_presence_of(:ending_at) }
+    it { should define_enum_for(:time_type).with_values([:self_improvement, :dayjob, :side_project]) }
+  end
+end
