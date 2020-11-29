4e6de7d0127f5389d8a1d79f648cb8c7eb6cc2be
Çağrı Özkan <cagrio.dev@gmail.com>
2020-11-30 00:19:51 +0300

    Add create method and add form on worklogs view

--- app/controllers/worklogs_controller.rb
+++ app/controllers/worklogs_controller.rb
@@ -4,4 +4,20 @@ class WorklogsController < ApplicationController
   def index
     @worklogs = current_user.worklogs.order(ending_at: :desc)
   end
+
+  def create
+    @worklog = Worklog.new(worklog_params)
+    @worklog.user_id = current_user.id
+    if @worklog.save
+      redirect_to worklogs_path, notice: 'Successfully saved!'
+    else
+      redirect_to worklogs_path, alert: @worklog.errors.full_messages.join(' - ')
+    end
+  end
+
+  private
+
+  def worklog_params
+    params.require(:worklog).permit(:time_type, :starting_at, :ending_at, :remark)
+  end
 end
