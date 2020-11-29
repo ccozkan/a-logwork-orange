a406cb02de0399aaa5f733e31cd3e5dcff15a0bd
Çağrı Özkan <cagrio.dev@gmail.com>
2020-11-30 00:11:38 +0300

    Validate consistency of time interval on worklog model and its tests

--- app/models/worklog.rb
+++ app/models/worklog.rb
@@ -3,7 +3,15 @@ class Worklog < ApplicationRecord
   validates_presence_of :starting_at
   validates_presence_of :ending_at
   validates_presence_of :time_type
+  validate :consistency_of_time_interval
+
   enum time_type: [ :self_improvement, :dayjob, :side_project ]
+
+  def consistency_of_time_interval
+    return if starting_at.nil? && ending_at.nil? || ending_at > starting_at
+
+    errors.add(:ending_at, 'ending_at must be greater than starting_at')
+  end
 
   def duration_in_minutes
     ((ending_at - starting_at) / 1.minute).to_i
--- spec/models/worklog_spec.rb
+++ spec/models/worklog_spec.rb
@@ -1,16 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Worklog, type: :model do
+  let(:user) { User.create(email: 'foo@bar.com', password: '123123')}
   describe 'model consistency' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to validate_presence_of(:starting_at) }
     it { is_expected.to validate_presence_of(:time_type) }
     it { is_expected.to validate_presence_of(:ending_at) }
     it { should define_enum_for(:time_type).with_values([:self_improvement, :dayjob, :side_project]) }
+
+    describe 'consistency_of_time_interval' do
+      let(:worklog) { Worklog.new(user_id: user.id, time_type: 1, starting_at: Time.current - 50.minutes)}
+      context 'invalid' do
+        it do
+          worklog.ending_at = Time.current - 1.hour
+          expect(worklog.valid?).to eq false
+          expect(worklog.errors.messages.values).to include ['ending_at must be greater than starting_at']
+        end
+      end
+      context 'valid' do
+        it do
+          worklog.ending_at = Time.current + 1.hour
+          expect(worklog.valid?).to eq true
+        end
+      end
+    end
   end
 
   describe '#duration_in_minutes' do
-    let(:user) { User.create(email: 'foo@bar.com', password: '123123')}
     let(:worklog) { Worklog.new(user_id: user.id, time_type: 1, starting_at: Time.current - 50.minutes, ending_at: Time.current)}
 
     it do
