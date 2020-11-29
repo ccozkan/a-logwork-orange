15e80b0e0a3341932481d8683a679f1e8707c2fc
Çağrı Özkan <cagrio.dev@gmail.com>
2020-11-30 01:55:05 +0300

    Fix factory

--- spec/factories/worklog.rb
+++ spec/factories/worklog.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
-
   factory :worklog do
     user { nil }
     remark { nil }
-    time_type { random_time_type }
-    starting_at { random_starting_at }
-    ending_at { random_ending_at }
+    time_type { 1 }
+    starting_at { Time.current - 2.hours }
+    ending_at { Time.current - 45.minutes }
   end
 end
