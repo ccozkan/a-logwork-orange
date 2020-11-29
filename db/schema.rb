e55ee86aaea889672cabe15c6d7dfdaf06fbbaa2
Çağrı Özkan <cagrio.dev@gmail.com>
2020-11-30 00:24:35 +0300

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_200236) do
+ActiveRecord::Schema.define(version: 2020_11_24_201421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -53,4 +39,16 @@ ActiveRecord::Schema.define(version: 2020_11_24_200236) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "worklogs", force: :cascade do |t|
+    t.string "remark"
+    t.integer "time_type"
+    t.datetime "starting_at"
+    t.datetime "ending_at"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_worklogs_on_user_id"
+  end
+
+  add_foreign_key "worklogs", "users"
 end
